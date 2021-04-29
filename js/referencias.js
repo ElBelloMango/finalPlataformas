881be7f3e809cd17958ce6226a214b534aa866ba
ElBelloMango <miguel.gallego@upb.edu.co>
2021-04-29 17:54:30 -0500

    Prueba 3 funcionamiento

--- js/referencias.js
+++ js/referencias.js
@@ -6,15 +6,14 @@ function init()
 {
     asignarVariables();
     asignarRutas();
-    alert(window.location.pathname);
     if(window.location.pathname=="/personajes.html"||window.location.pathname=="/finalPlataformas/personajes.html")
     {
         rotacion();
     }
     if(window.location.pathname=="/index.html" || window.location.pathname=="/"||window.location.pathname=="/finalPlataformas/index.html"||window.location.pathname=="/finalPlataformas/")
     {
-        logeado=false;
-        logeo();
+        logeado=true;
+        // logeo();
     }
     if(window.location.pathname=="/renderizarAPI.html"||window.location.pathname=="/finalPlataformas/renderizarAPI.html")
     {
